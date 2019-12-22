9d45db97c6a357df654f95bab9dfc25c706bfe49
yhpp <yhpp1995@gmail.com>
2019-12-22 14:18:53 +0800

    build: add todo-item component

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  tests = [{ item: 1 }, { item: 2 }];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
